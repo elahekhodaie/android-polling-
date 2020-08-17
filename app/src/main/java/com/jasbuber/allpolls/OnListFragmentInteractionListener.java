7508c7cce9eaf94f92f128c23195b9b2ed7742c9
ellykhodaie <elahekhodaie@yahoo.com>
2020-08-17 10:02:31 +0430

    first commit

---
+++ app/src/main/java/com/jasbuber/allpolls/OnListFragmentInteractionListener.java
@@ -0,0 +1,7 @@
+package com.jasbuber.allpolls;
+
+import com.jasbuber.allpolls.models.Poll;
+
+public interface OnListFragmentInteractionListener {
+    void onListFragmentInteraction(Poll poll);
+}
---
+++ app/src/main/java/com/jasbuber/allpolls/OnListMyPollsInteractionListener.java
@@ -0,0 +1,6 @@
+package com.jasbuber.allpolls;
+
+import com.jasbuber.allpolls.models.orm.PollORM;
+public interface OnListMyPollsInteractionListener {
+    void onListFragmentInteraction(PollORM poll);
+}
